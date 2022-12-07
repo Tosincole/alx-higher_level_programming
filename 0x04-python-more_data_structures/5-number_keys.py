dff5ccacc343a29b45de0155576c4c9531dc1db6
Tosin <Tosincole95@gmail.com>
2022-12-07 22:47:48 +0000

    Task 5 through Task 12

--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -6,11 +6,4 @@ def number_keys(a_dictionary):
     for i in list_keys:
         num += 1
 
-    r_dictionary):
-    num = 0
-    list_keys = list(a_dictionary.keys())
-
-    for i in list_keys:
-        num += 1
-
-    return (num)eturn (num_
+    return (num)
